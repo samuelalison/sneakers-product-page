58a484d02beb70b2ef8d8bd8f4109a5b24956594
samuelalison <samuelalison13@gmail.com>
2024-03-26 12:40:09 +0100

    added the removeFromCart feature

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,29 +2,48 @@ import { CartContext } from '@/contexts/CartContext';
 import React, { useContext } from 'react'
 import Image from 'next/image';
 
+import { ProductsContext } from '@/contexts/ProductContext';
+
+
 const CartItem = ({ cartItem }) => {
-const {quantity} = useContext(CartContext)
+  const { products } = useContext(ProductsContext);
+const {quantity, removeCartItem} = useContext(CartContext);
+const {setQuantity} = useContext(CartContext);
 const {name, imageURL, price } = cartItem;
 
-  return (
-    <div className='flex gap-4'>
 
+const handleItemRemoval = () => {
+  setQuantity(q => q = 0)
+  removeCartItem(products[0].id);
+};
+
+  return (
+    <div className=' gap-4'>
+
+
+<div className='flex gap-2'>
     <div className=''>
       <Image src={imageURL} alt='unknown' width={50} height={50}/>
     </div>
 
-    <div className=''>
+  
     <div className=''>
       <h2 className='text-black-300 text-neutral-grayish-blue font-sans'>{name}</h2>
     </div>
 
+<div className="cursor-pointer" onClick={handleItemRemoval}>
+  <Image alt='delete-icon'  src="/icon-delete.svg" width={30} height={30} />
+</div>
 
-    <div className='flex gap-2'>
+    </div>
+
+    
+    
+    <div className='flex gap-2 justify-center'>
       <p>${price}</p>
       <p>*</p>
       <p>{quantity}</p>
       <p><span>$</span>{price * quantity}</p>
-    </div>
     </div>
     </div>
   )
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,67 +1,3 @@
-// import { createContext, useState, useContext } from 'react';
-// import { ProductsContext } from '@/contexts/ProductContext';
-
-// export const CartContext = createContext({
-//   isCartOpen: false,
-//   setIsCartOpen: () => { },
-//   cartItems: [],
-//   addToCart: () => { },
-//   removeCartItem: () => { },
-//   decrementCartItem: () => { },
-// });
-
-// export const CartProvider = ({ children }) => {
-//   const { products } = useContext(ProductsContext);
-//   const [isCartOpen, setIsCartOpen] = useState(false);
-//   const [cartItems, setCartItems] = useState([]);
-
-//   const addToCart = (productId, quantityToAdd) => {
-
-//     const productToAdd = products.find(product => product.id === productId);
-//     if (!productToAdd) {
-//       return;
-//     }
-
-//     const existingCartItemIndex = cartItems.findIndex(item => item.id === productId);
-//     if (existingCartItemIndex !== -1) {
-//       setCartItems(prevCartItems => {
-//         const updatedCartItems = prevCartItems.map((item, index) =>
-//           index === existingCartItemIndex ? { ...item, quantity: item.quantity + quantityToAdd } : item
-//         );
-//         return updatedCartItems;
-//       });
-//     } else {
-//       setCartItems(prevCartItems => {
-//         const newCartItems = [...prevCartItems, { ...productToAdd, quantity: quantityToAdd }];
-//         return newCartItems;
-//       });
-//     }
-//   };
-
-//   const decrementCartItem = (productId) => {
-//     const existingCartItemIndex = cartItems.findIndex(item => item.id === productId);
-//     if (existingCartItemIndex !== -1 && cartItems[existingCartItemIndex].quantity > 1) {
-//       setCartItems(prevCartItems => {
-//         const updatedCartItems = [...prevCartItems];
-//         updatedCartItems[existingCartItemIndex].quantity -= 1;
-//         return updatedCartItems;
-//       });
-//     } else {
-//       removeCartItem(productId);
-//     }
-//   };
-
-//   const removeCartItem = (productId) => {
-//     setCartItems(prevCartItems => prevCartItems.filter(item => item.id !== productId));
-//   };
-
-//   const value = { isCartOpen, setIsCartOpen, cartItems, addToCart, removeCartItem, decrementCartItem };
-
-//   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
-// };
-
-
-
 import { createContext, useState, useContext } from 'react';
 import { ProductsContext } from '@/contexts/ProductContext';
 
@@ -71,6 +7,7 @@ export const CartContext = createContext({
   cartItems: [],
   addToCart: () => { },
   removeCartItem: () => { },
+  // deleteCartItem: () => { },
   decrementCartItem: () => { },
 });
 
@@ -120,7 +57,17 @@ export const CartProvider = ({ children }) => {
     setCartItems(prevCartItems => prevCartItems.filter(item => item.id !== productId));
   };
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addToCart, removeCartItem, decrementCartItem, quantity, setQuantity};
+  // const deleteCartItem = (itemToDelete) => {
+  //   const updatedCartItems = cartItems.map(item => {
+  //     if (item === itemToDelete) {
+  //       return { ...item, quantity: 0 };
+  //     }
+  //     return item;
+  //   });
+  //   setCartItems(updatedCartItems);
+  // }
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addToCart, removeCartItem, decrementCartItem, quantity, setQuantity };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
